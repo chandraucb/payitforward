204c0483fbadd90c54b93b43e27ea49eff81d138
Junel <robin.lennuj@gmail.com>
2023-10-07 16:31:12 -0700

    Added Post model. First edit.

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const Tech = require('./Tech');
 const Matchup = require('./Matchup');
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { Tech, Matchup, User };
+module.exports = { Tech, Matchup, User, Post };
