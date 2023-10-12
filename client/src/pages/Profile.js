c0aac95092119af8aa09b038b4bb2975272849d7
Junel <robin.lennuj@gmail.com>
2023-10-11 19:41:26 -0700

    Worked on Profile.js for pages & profileSeeds.json.

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import profiles from '../../server/seeds/profileSeeds.json';
+
+class Profile extends React.Component {
+  render() {
+    const profileData = profiles[0]; // If we want to display the first profile or we can change it to display all profile? 
+
+    return (
+      <div>
+        <h1>Profile</h1>
+        <div>
+          <img
+            src={profileData.profilePicture} // Will need actual URL for the profile picture.
+            alt="Profile Picture"
+            style={{ width: '150px', borderRadius: '50%' }}
+          />
+          <h2>Name: {profileData.name}</h2>
+          <p>Bio: {profileData.bio}</p>
+          <p>Projects: {profileData.projects}</p>
+          <p>Organizations: {profileData.organizations}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
+
+
+// First edit to Profile.js. I'm still unsure if I did this correctly. Needs more work done.
