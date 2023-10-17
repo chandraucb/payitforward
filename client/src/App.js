c2258292d8004fe1e68512484592b44bf2d6c0f9
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-10-16 22:57:08 -0700

    store changes

--- client/src/App.js
+++ client/src/App.js
@@ -48,11 +48,11 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={<Login />}
             />
             <Route
-              path="/login"
-              element={<Login />}
+              path="/Home"
+              element={<Home />}
             />
             <Route
               path="/Signup"
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link className="text-light" to="/">
+        <Link to="/">
           <img src={Logo} alt="pay it logo"></img>
         </Link>
         {Auth.loggedIn() ? (
