f4790dab5d30d691f6b56856efca33915bd9c9de
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-10-16 20:59:54 -0700

    moved images ot inside of src added logo to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import Logo from '../../images/pay_it_logo-transformed.png'
 
 const Header = () => {
   const logout = event => {
@@ -11,7 +12,7 @@ const Header = () => {
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link className="text-light" to="/">
-          <h1 className="m-0">Tech Thoughts</h1>
+          <img src={Logo} alt="pay it logo"></img>
         </Link>
         {Auth.loggedIn() ? (
           <div>
