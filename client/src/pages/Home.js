f77051a68cb0f3434c375b3aebe252e13329cb77
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-10-18 22:08:36 -0700

    added import to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Scheduler } from "@aldabil/react-scheduler";
 // Uncomment import statements below after building queries and mutations
 
 const Home = () => {
-  //const { data } = useQuery(QUERY_MATCHUPS);
-  //const matchupList = data?.matchups || [];
-
   return (
     <div className="card bg-white card-rounded w-50">
       <div className="card-header bg-dark text-center">
